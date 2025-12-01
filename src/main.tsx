b28f8da0f01f9a65916fc3a9e024033689cef25f
jayant99acharya <jayant99acharya@gmail.com>
2025-12-01 15:39:21 +0530

    added web3

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './lib/web3';
 import './lib/escrow-helper';
 import './lib/contract-fix';
 
